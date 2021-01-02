3c61017b4696916e37634dd5ca67735fa90ed4ae
Huzaifa Raghav <windowpane1712@gmail.com>
2021-01-02 15:30:51 +0800

    Part 21 done(Bottom Sheets)

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -10,6 +10,19 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // this function is here as it needs access to the BuildContext
+    // builder returns a widget tree that is shown when the bottom sheet is triggered
+    void _showSettingsPanel(){
+      showModalBottomSheet(context: context,
+          builder: (context) {
+            return Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+              child: Text("Bottom Sheet"),
+            );
+          }
+      );
+    }
+
     return Container(
       child: StreamProvider<List<BrewModel>>.value(
         value: DatabaseService().brews,
@@ -19,13 +32,21 @@ class Home extends StatelessWidget {
             title: Text("Brew Crew"),
             backgroundColor: Colors.brown[400],
             actions: <Widget>[
+              // Log out
                FlatButton.icon(
                  icon: Icon(Icons.person),
                  label: Text("Log out"),
                  onPressed: () async {
                   await _auth.signOut();
                  },
-               )
+               ),
+              // Settings button
+              FlatButton.icon(
+                icon: Icon(Icons.settings),
+                label: Text("Settings"),
+                // this function triggers showing of the bottom sheet
+                onPressed: () => _showSettingsPanel(),
+              )
             ],
           ),
           body: BrewList(),
